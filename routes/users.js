826b2bfe20ce7cf93f71a0fe2c2b67edc0165953
Akash Prasher <prince.akash.prasher@gmail.com>
2021-12-30 13:26:53 +0530

    adding flash messages and logout route
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,12 @@ var router = express.Router();
 var User = require("../models/User");
 var bcrypt = require("bcrypt");
 
+var userObj = { name: "", email: "" };
+
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.render("users");
+  console.log(req.session.userId);
+  res.render("users", { userObj });
 });
 
 router.get("/register", (req, res, next) => {
@@ -14,7 +17,8 @@ router.get("/register", (req, res, next) => {
 });
 
 router.get("/login", (req, res, next) => {
-  res.render("login");
+  var error = req.flash("error")[0];
+  res.render("login", { error });
 });
 
 router.post("/register", (req, res, next) => {
@@ -27,21 +31,38 @@ router.post("/register", (req, res, next) => {
 router.post("/login", (req, res, next) => {
   var { email, password } = req.body;
   if (!email && !password) {
-    res.redirect("/users/login");
+    req.flash("error", "Email and Pasword Required");
+    return res.redirect("/users/login");
   }
 
   User.findOne({ email }, (err, user) => {
     if (err) return next(err);
     if (!user) {
-      res.redirect("/users/login");
+      req.flash("error", "Email isn't registered");
+      return res.redirect("/users/login");
     }
     user.verifyPassword(password, (err, result) => {
       if (err) return next(err);
       if (!result) {
-        res.redirect("/users/login");
+        req.flash("error", "Please Enter right password");
+        return res.redirect("/users/login");
       }
+      // Create a session
+      req.session.userId = user.id;
+      // userObj.name = user.name;
+      // userObj.email = user.email;
+      res.redirect("/users");
+      console.log(req.session.userId);
     });
   });
+});
+
+router.get("/logout", (req, res, next) => {
+  req.session.destroy();
+  // res.clearCookie("connect.sid");
+  // userObj.email = "";
+  // userObj.name = "";
+  res.redirect("/");
 });
 
 module.exports = router;
