32f57a3b80e218d7e0bbe4a7ff48ff5d8d5141c9
Akash Prasher <prince.akash.prasher@gmail.com>
2022-01-06 12:28:57 +0530

    Adding Books Module along with protected routed
    Signed-off-by: Akash Prasher <prince.akash.prasher@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
+});
+
+router.get("/protected", (req, res, next) => {
+  console.log(req.session);
+
+  if (req.session && req.session.userId) {
+    res.send("PROTECTED CONTENT");
+  } else {
+    res.redirect("/users/login");
+  }
 });
 
 module.exports = router;
